d5b27bf93e8ed56a0845666aba0e0159b8b160e9
Jeremy Sim <jsim0809@gmail.com>
2020-07-07 11:32:13 -0700

    Set up XState

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useMachine } from '@xstate/react';
+import woosterMachine from './woosterMachine.js';
 
 function App() {
-  return (
-    <div>Welcome to Wooster</div>
-  );
+  const [currentState, sendEvent] = useMachine(woosterMachine);
+
+  useEffect(() => {
+    setTimeout(() => {
+      sendEvent('ANIMATION_DONE');
+    }, 3000);
+  }, []);
+
+  if (currentState.matches('intro')) {
+    return (
+      <div>Welcome to Wooster</div>
+    );
+  } else if (currentState.matches('landing')) {
+    return (
+      <div>Log in</div>
+    );
+  }
 }
 
 export default App;
---
+++ client/woosterMachine.js
@@ -0,0 +1,31 @@
+import { Machine } from 'xstate';
+
+// This machine is completely decoupled from React
+const woosterMachine = Machine({
+  id: 'wooster',
+  initial: 'intro',
+  states: {
+    intro: {
+      on: {
+        ANIMATION_DONE: 'landing',
+      }
+    },
+    landing: {
+      on: {
+        LOGIN_CLICK: 'loggedInNotPlaying',
+      }
+    },
+    loggedInNotPlaying: {
+      on: {
+        PLAY_CLICK: 'loggedInPlaying',
+      }
+    },
+    loggedInPlaying: {
+      on: {
+        PAUSE_CLICK: 'loggedInNotPlaying',
+      }
+    },
+  },
+});
+
+export default woosterMachine;
