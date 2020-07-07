409ae22ae6c140fc7832964e612d506af2734177
Jeremy Sim <jsim0809@gmail.com>
2020-07-07 08:14:56 -0700

    Switch to React setup and initialize webpack/babel

---
+++ client/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>Welcome to Wooster</div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'client', 'index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  mode: 'development'
+};
