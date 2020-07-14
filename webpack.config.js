d1515155ef3bbae0b93663cd5c34afd6b8e6fbc7
Jeremy Sim <jsim0809@gmail.com>
2020-07-14 09:24:30 -0700

    Complete intro animation timings

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: path.resolve(__dirname, 'client', 'index.jsx'),
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.js',
   },
   module: {
