a34ef4f567339c4779e98dbbf96e3b7aa54ad9fb
Jeremy Sim <jsim0809@gmail.com>
2020-07-16 21:21:16 -0700

    Complete fadeout animation

--- client/App.jsx
+++ client/App.jsx
@@ -9,7 +9,6 @@ function App() {
   const [currentState, sendEvent] = useMachine(woosterMachine);
 
   useEffect(() => {
-    // document.querySelector('#wipe-animation').style.width = '523px';
     // setTimeout(() => {
     //   sendEvent('ANIMATION_DONE');
     // }, 3000);
