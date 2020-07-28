d61ab484491d07a99d2ce049391c00354d92854d
Jeremy Sim <jsim0809@gmail.com>
2020-07-28 09:49:11 -0700

    Begin setup of DynamoDB

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const axios = require('axios');
-const secret = require('./secret.keys.js');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const queryString = require('query-string');
+
+const dynamodb = require('dynamodb');
+
+const secret = require('./secret.keys.js');
 
 const app = express();
 const PORT = 1234;
