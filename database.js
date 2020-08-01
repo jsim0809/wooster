1d72acd377fb440684439a7d9a4b932759b401d1
Jeremy Sim <jsim0809@gmail.com>
2020-08-01 07:38:51 -0700

    Switch to AWS guide instead of using dynamodb package and joi

--- database.js
+++ database.js
@@ -1,21 +1,55 @@
-const dynamo = require('dynamodb');
-const joi = require('joi');
+const AWS = require('aws-sdk');
 const secret = require('./secret.keys.js');
 
-dynamo.AWS.config.update({
+AWS.config.update({
   accessKeyId: secret.AWS_ACCESS_KEY_ID,
   secretAccessKey: secret.AWS_SECRET_ACCESS_KEY,
   region: 'us-west-2',
-})
+  endpoint: '',
+});
 
-const Account = dynamo.define('Account', {
-  hashKey: 'email',
-  timestamps: true,
-  schema: {
-    email: joi.string(),
-    spotify_id: joi.string(),
-    songs: {
-      
-    }
+// Create a table
+const dynamodb = new AWS.DynamoDB();
+
+const params = {
+  TableName: "Wooster",
+  KeySchema: [
+    { AttributeName: "email", KeyType: "HASH" }
+  ],
+  AttributeDefinitions: [
+    { AttributeName: "email", AttributeType: "S" }
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 0,
+    WriteCapacityUnits: 0
   }
-})
+};
+
+dynamodb.createTable(params, (err, data) => {
+  if (err) {
+      console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+  } else {
+      console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+  }
+});
+
+
+// Write new user to table
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const writeData = (email, spotify_user_id) => {
+  const params = {
+    TableName: "Wooster",
+    Item: {
+        "email": email,
+        "spotify_user_id": spotify_user_id,
+    },
+  };
+  docClient.put(params, function(err, data) {
+    if (err) {
+        console.error("Unable to add user", email, ". Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("New user added:", email);
+    }
+ });
+};
