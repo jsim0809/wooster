1f2fe3c3d999e2ca901d218a2e09bf256d2a3b2b
Jeremy Sim <jsim0809@gmail.com>
2020-08-17 14:00:51 -0700

    Place featured song display

--- client/Main.jsx
+++ client/Main.jsx
@@ -5,8 +5,20 @@ function Main() {
     <main>
       <div id="logged-in-text">Logged in as jsim0809 (Log out)</div>
       <div id="now-playing-text">Now Playing</div>
-      <div id="song-box">Song info here</div>
-      <div id="control-bar">Play bar here</div>
+      <div id="song-box">
+        <img src="assets/album-image-example.jpg" />
+        <div id="song-box-text">
+          <div id="song-title">I Like It Like That</div>
+          <div id="song-artists">Hot Chelle Rae</div>
+        </div>
+      </div>
+      <div id="control-bar">
+        <div>Sleep</div>
+        <div>Dislike</div>
+        <div>Play</div>
+        <div>Like</div>
+        <div>Woo!</div>
+      </div>
     </main> 
   )
 }
