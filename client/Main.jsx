76ec44d65400619d1f742e40f3a449d66bce5d60
Jeremy Sim <jsim0809@gmail.com>
2020-08-17 11:24:54 -0700

    Place logged-in text

--- client/Main.jsx
+++ client/Main.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 function Main() {
   return (
-    <main>Now Playing</main> 
+    <main>
+      <div id="logged-in-text">Logged in as jsim0809</div>
+      <div id="now-playing-text">Now Playing</div>
+      <div id="song-info-box">Song info here</div>
+      <div id="control-bar">Play bar here</div>
+    </main> 
   )
 }
 
