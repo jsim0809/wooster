c6e9d21012d75f11c9304b4b953ba36e5f115f4a
Jeremy Sim <jsim0809@gmail.com>
2020-08-16 21:44:34 -0700

    Add and position navbar elements

--- client/Sidebar.jsx
+++ client/Sidebar.jsx
@@ -3,9 +3,17 @@ import React from 'react';
 function Sidebar() {
   return (
     <nav>
-      <img id="wooster-music" src="assets/wooster-music.svg" alt="Wooster Music"/>
+      <img id="wooster-music" src="assets/wooster-music.svg" alt="Wooster Music" />
+      <ul>
+        <li>Music Player</li>
+        <li>Your Music</li>
+        <li>What is Wooster?</li>
+        <li>About / Contact</li>
+      </ul>
+      <div id="dark-mode-button">
+        Dark mode
+      </div>
     </nav>
- 
   )
 }
 
