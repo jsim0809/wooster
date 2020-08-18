47f27f89dc87b92c93269cc06d5e78812a8c0edc
Jeremy Sim <jsim0809@gmail.com>
2020-08-18 11:42:23 -0700

    Finish laying out login page

--- client/LandingPage.jsx
+++ client/LandingPage.jsx
@@ -4,16 +4,16 @@ function LandingPage() {
   return (
     <main>
       <div id="sample-ui">
-        <div id="logged-in-text" style={{visibility: 'hidden'}}>Logged in as jsim0809 (Log out)</div>
-        <div id="now-playing-text">Now Playing</div>
-        <div id="song-box">
+        <div id="logged-in-text" style={{ visibility: 'hidden' }}>Logged in as samantha (Log out)</div>
+        <div id="main-title" className="blurred">Now Playing</div>
+        <div id="song-box" className="blurred">
           <img src="assets/album-image-example.jpg" />
           <div id="song-box-text">
             <div id="song-title">I Like It Like That</div>
             <div id="song-artists">Hot Chelle Rae</div>
           </div>
         </div>
-        <div id="control-bar">
+        <div id="control-bar" className="blurred">
           <img className="control-bar-dislike" src="assets/dislike.svg" />
           <img className="control-bar-skip-back" src="assets/skip-back.svg" />
           <img className="control-bar-play-pause" src="assets/play.svg" />
@@ -23,7 +23,14 @@ function LandingPage() {
         </div>
       </div>
       <div id="login">
-        Log in with Spotify
+        <div id="logged-in-text" style={{ visibility: 'hidden' }}>Logged in as samantha (Log out)</div>
+        <div id="main-title">Welcome to Wooster Music Player</div>
+        <div id="login-box">
+          <div id="main-subtitle">To get started, log in with your Spotify Premium account.</div>
+          <img src="assets/login-button-rectangular.svg" alt="Log in with Spotify"/>
+          <div></div>
+        </div>
+
       </div>
     </main>
   );
