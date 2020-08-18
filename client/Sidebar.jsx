4624824b7fbbb182b6bd6f748ca10774178a2e25
Jeremy Sim <jsim0809@gmail.com>
2020-08-18 13:27:12 -0700

    Set up hover functionality for landing page

--- client/Sidebar.jsx
+++ client/Sidebar.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Sidebar() {
+  const [dimmerArray, setDimmerArray] = useState(['', '', '', '']);
+
+  const handleMouseEnter = (event) => {
+    setDimmerArray(dimmerArray.map((dimmer, index) => {
+      if (index == event.currentTarget.id) {
+        return '';
+      } else {
+        return 'dimmed'
+      }
+    }));
+  };
+
+  const handleMouseLeave = (event) => {
+    setDimmerArray(['', '', '', '']);
+  };
+
   return (
     <nav>
       <img id="wooster-music" src="assets/wooster-music.svg" alt="Wooster Music" />
       <ul>
-        <li>Music Player</li>
-        <li>Your Music</li>
-        <li>What is Wooster?</li>
-        <li>About / Contact</li>
+        <li id="0" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={dimmerArray[0]}>Music Player</li>
+        <li id="1" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={dimmerArray[1]}>Your Music</li>
+        <li id="2" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={dimmerArray[2]}>What is Wooster?</li>
+        <li id="3" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={dimmerArray[3]}>About / Contact</li>
       </ul>
       <div id="dark-mode-text">
         Dark mode
