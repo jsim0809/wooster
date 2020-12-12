9826ffa9618c3ddf3950ac0f65000417961e4ed3
Jeremy Sim <jsim0809@gmail.com>
2020-12-11 22:31:56 -0800

    Change back to mouseEnter to avoid jerky behavior

--- client/SearchBar.jsx
+++ client/SearchBar.jsx
@@ -81,7 +81,7 @@ function SearchBar({
     }
   };
 
-  const handleMouseOver = (event) => {
+  const handleMouseEnter = (event) => {
     setSelectedIndex(Number(event.currentTarget.getAttribute('name')));
   }
 
@@ -105,7 +105,7 @@ function SearchBar({
         className={selectedIndex === index + 1 ? 'selected-song' : ''}
         key={index + 1}
         name={index + 1}
-        onMouseOver={handleMouseOver}
+        onMouseEnter={handleMouseEnter}
         onClick={handleSubmit}>
         {`${pluralize(result.artists)} – ${result.name}`}
       </div>
